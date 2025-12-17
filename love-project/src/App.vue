183437f18c0d58ee0b4e0e8889e4e277a0151f01
TommDevv <162940383+TommDevv@users.noreply.github.com>
2025-12-17 00:12:33 -0500

    prueba del repo

--- love-project/src/App.vue
+++ love-project/src/App.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from 'vue'
 </script>
 
 <template>
   <div class="center-title">
-    <p>El componente funciona</p>
+    <p>El componente funcionaa</p>
   </div>
 </template>
 
 <style scoped>
-.center-title{
-  font-weight: bold;
-  font-size: 24px;
-  padding: 20px;
-  text-align: center;
-  display:flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-}
+  .center-title{
+    font-weight: bold;
+    font-size: 24px;
+    padding: 20px;
+    text-align: center;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
 </style>
