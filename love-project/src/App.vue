f5bdad2a966e27c49a8bbaf035da0bfed63e9f96
TommDevv <162940383+TommDevv@users.noreply.github.com>
2025-12-17 00:08:03 -0500

    Primer commit del proyecto para mi amor

---
+++ love-project/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="center-title">
+    <p>El componente funciona</p>
+  </div>
+</template>
+
+<style scoped>
+.center-title{
+  font-weight: bold;
+  font-size: 24px;
+  padding: 20px;
+  text-align: center;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
