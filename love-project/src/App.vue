b8b058fb95cbe915d97324c633c8c485713bf9ca
TommDevv <tomasonic2@gmail.com>
2025-12-23 00:35:18 -0500

    avances

--- love-project/src/App.vue
+++ love-project/src/App.vue
@@ -1,36 +1,79 @@
 <script setup lang="ts">
   import { ref } from 'vue'
+  import StarrySkyBackground from './components/starry-sky.vue'
+  const showConstLines = ref(false);
+
 </script>
 
 <template>
-  <p>HOLA MUNDO</p>
+  <div>
+      <StarrySkyBackground :showConstellation="showConstLines">
+      <h1 class="title-container twinkle-star-regular">
+        <span class="blur">Titulo Provisional</span>
+        <span class="text">Titulo Provisional</span>
+      </h1>
+    </StarrySkyBackground>
+
+    <div class="ground"></div>
+  </div>
 </template>
 
-<style scoped>
-  .sky-layer {
-    position: fixed;
-    inset: 0;
-    background-image: url('assets/sky/background.png');
-    background-size: cover;
-    background-position: center 0%;
-    z-index: -10;
-    pointer-events: none;
-  }
+<style>
 
-  .ground-layer {
-    position: relative;
-    z-index: 1;
+  .scene-container{
+    position:relative;
     min-height: 200vh;
-    background-image: url('assets/ground/ground.png');
+    width: 100%;
   }
 
-  .chest-layer {
-    width: 400px;
-    margin: 0 auto;
-    margin-top: 50vh;
+  .spacer{
+    height: 100vh;
   }
 
-  .spacer {
-  height: 100vh;
+  .ground{
+    position: sticky;
+    bottom: 0;
+    height:600px;
+    width: 100vw;
+    margin-left: calc(-50vw + 50%);
+    display: block;
+    background-image: url('./assets/ground/vecteezy_graphics-design-grass-on-white-background-vector_8071361-Photoroom.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center bottom;
+    z-index: 10;
+    object-fit: cover;
   }
+
+    html, body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+
+  }
+
+  .title-container {
+    position: relative;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding-top:20px;
+  }
+
+  .title-container span{
+    position:absolute;
+  }
+
+  .blur{
+    filter: blur(6px);
+    opacity: 0.8;
+    color:rgb(230, 144, 241)
+  }
+
+  .text {
+    position: relative;
+  }
+
 </style>
--- love-project/src/components/starry-sky.vue
+++ love-project/src/components/starry-sky.vue
@@ -1,32 +1,119 @@
 <template>
     <div class="starry-wrapper">
+        <!-- fondo del cielo -->
         <div class="sky"></div>
 
         <!-- estrellas -->
-         <div class="stars stars-1"></div>
-         <div class="stars stars-1"></div>
+         <div class="stars stars-1" :style="starsFastStyle"></div>
+         <div class="stars stars-2" :style="starsSlowStyle"></div>
 
          <!-- contenido principal -->
-          <div class="content">
+          <div class="content" :style="contentStyle">
             <slot></slot>
+          </div>
+
+          <div class="constellation-wrapper" :style="constWrapperStyle">
+                        <!-- Constelacion lineas -->
+            <svg class="constellation-links" viewBox="0 0 100 100" preserveAspectRatio="none" v-if="props.showConstellation">
+                <line
+                    v-for="(link, index) in constellationLinks"
+                    :key="index"
+                    :x1="getPoint(link[0])?.x || 0"
+                    :y1="getPoint(link[0])?.y || 0"
+                    :x2="getPoint(link[1])?.x || 0"
+                    :y2="getPoint(link[1])?.y || 0"
+                    vector-effect="non-scaling-stroke"
+                    pathLength="1"
+                />
+            </svg>
+                      <!-- constelacion estrellas-->
+            <div 
+                v-for="point in constellationPoints"
+                :key="point.id"
+                class="constellation-button"
+                :style="{
+                    left: point.x + '%',
+                    top: point.y + '%'
+                }"
+             ></div>
           </div>
     </div>
 </template>
 
 <script setup lang="ts">
+    import { onBeforeUnmount, onMounted, ref, computed } from 'vue';
+
     defineOptions({
         name: 'StarrySkyBackground'
-    })
+    });
+    
+    const props = defineProps<{
+        showConstellation?: boolean;
+    }>();
+
+    const scrollY = ref<number>(0);
+    //listener para cuando el uruario haga scroll
+    const onScroll = () => {
+        scrollY.value = window.scrollY;
+    };
+
+    //modificadores del scroll para efectuar estilo "parallax"
+    const starsSlowStyle = computed(() => ({
+        transform: `translateY(${-scrollY.value * 0.1}px)`
+    }));
+
+    const starsFastStyle = computed(() => ({
+        transform: `translateY(${-scrollY.value * 0.18}px)`
+    }));
+
+    const contentStyle = computed(() => ({
+        transform: `translateY(${scrollY.value * 0.25}px)`
+    }));
+
+    const constWrapperStyle = computed(() => ({
+        transform: `translateY(${-scrollY.value * 0.05}px)`
+    }));
+
+    onMounted(() => {
+        window.addEventListener('scroll', onScroll, {passive: true});
+    });
+
+    onBeforeUnmount(() => {
+        window.removeEventListener('scroll', onScroll);
+    });
+
+    function getPoint(id:number | undefined): {id:number, x:number, y:number} | undefined {
+        return constellationPoints.value.find(p => p.id === id); 
+    }
+
+    //Referencia de posicion de los puntos de las constelaciones
+    const constellationPoints = ref<Array<{id:number, x:number, y:number}>>([
+        {id:1, x:37, y:22},
+        {id:2, x:50, y:30},
+        {id:3, x:63, y:22},
+        {id:4, x:67, y:40},
+        {id:5, x:33, y:40},
+        {id:6, x:50, y:70},
+    ]);
+
+    //referencia de los puntos de conexion entre las lineas SVG de la constelacion
+    const constellationLinks = [
+        [2,3],
+        [3,4],
+        [4,6],
+        [6,5],
+        [5,1],
+        [1,2]
+    ];
+
 </script>
 
-<style scooped>
+<style>
     /* contenedor principal */
     .starry-wrapper {
         position: relative;
-        pointer-events: none;
-        overflow:hidden;
         width: 100vw;
-        height: 100vh;
+        min-height: 70vh;
     }
 
     /*Fondo del cielo*/
@@ -36,10 +123,135 @@
         inset: 0;
         background: radial-gradient(
             ellipse at bottom,
-                #0b1d3a 0%,
-                #020111 70%,
+                #0d2242 0%,
+                #0e0111 70%,
                 #000000 100%
         );
         z-index: -1;
+        will-change: transform;
     }
+
+    /* Estrellas */
+    .stars {
+        position: fixed;
+        inset: 0;
+        background-repeat: repeat;
+        pointer-events: none;
+        will-change: transform, opacity;
+    }
+
+    .stars-1 {
+        z-index:2;
+        background-image:
+        radial-gradient(1.2px 2px at 20% 30%, white, transparent),
+        radial-gradient(1.2px 2px at 60% 40%, white, transparent),
+        radial-gradient(1.2px 2px at 50% 70%, white, transparent),
+        radial-gradient(1.2px 2px at 10% 80%, white, transparent),
+        radial-gradient(1.2px 2px at 70% 80%, white, transparent);
+        background-size: 220px 220px;
+        animation: twinkleSlow 3s infinite alternate;
+    }
+
+    .stars-2 {
+        z-index: 3;
+        background-image:
+        radial-gradient(1.2px 2px at 30% 20%, white, transparent),
+        radial-gradient(1.2px 2px at 80% 80%, white, transparent),
+        radial-gradient(1.2px 2px at 70% 50%, white, transparent),
+        radial-gradient(1.2px 2px at 90% 30%, white, transparent),
+        radial-gradient(1.2px 2px at 10% 60%, white, transparent),
+        radial-gradient(1.2px 2px at 50% 80%, white, transparent);
+        background-size: 150px 150px;
+        animation: twinkleFast 1.5s infinite alternate;
+
+    }
+
+    /*constelacion - botones*/
+    .constellation-wrapper {
+        position: fixed;
+        inset: 0;
+        z-index: 7;
+        margin-top: 10vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        pointer-events: none;
+    }
+
+    .constellation-button {
+        position:absolute;
+        width: 12px;
+        height: 12px;
+        border-radius: 50%;
+        transform: translate(-50%, -50%);
+        background-image: radial-gradient(10px 10px at center, white, rgb(134, 58, 131) 40%, transparent 60%);
+        animation: twinkleConstellation 2s infinite alternate;
+        pointer-events: auto;
+    }
+
+    .constellation-button:hover {
+        cursor: pointer;
+        box-shadow: 0 0 8px 4px rgb(134, 58, 131);
+        background-image: radial-gradient(10px 10px at center, white, rgb(134, 58, 131) 40%);
+    }
+
+    /*Constelacion - lineas*/
+
+    .constellation-links {
+        position: absolute;
+        inset: 0;
+        width: 100%;
+        height: 100%;
+        pointer-events: none;
+    }
+
+    .constellation-links line {
+        stroke: rgba(168, 86, 223, 0.4);
+        stroke-width: 1;
+        stroke-linecap: round;
+        filter: blur(0.1px);
+        stroke-dasharray: 100;
+        stroke-dashoffset: 100;
+        animation: drawLine 20s ease forwards;
+    }
+    
+    /* animaciones */
+    @keyframes twinkleSlow{
+        from {opacity: 0.4;}
+        to {opacity: 1;}
+    }
+
+    @keyframes twinkleFast{
+        from{opacity:0.2}
+        to{opacity:0.9}
+    }
+
+    @keyframes twinkleConstellation{
+        from{opacity:0.7}
+        to{opacity:1}
+    }
+
+    @keyframes drawLine {
+        to {
+            stroke-dashoffset: 0;
+        }
+    }
+
+
+    /*contenido principal */
+    .content {
+        position: relative;
+        z-index: 5;
+        height: 100%;
+    }
+
+
+    html, body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    }
+
+
+
 </style>
